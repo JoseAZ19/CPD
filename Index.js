1984fe0352dba79d3b0d8c55c2ad9a8d471bdbe2
Jose Azofeifa <jazofeifazeledon@gmail.com>
2024-08-21 22:44:23 -0600

    Changes

--- Index.js
+++ Index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+
+// Usar el puerto proporcionado por el entorno o el 80 por defecto
+const port = process.env.PORT || 80;
 
 // Definir la carpeta "public" como la carpeta para archivos estáticos
 app.use(express.static(path.join(__dirname, 'Public')));
 
 // Ruta para servir la página HTML
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Public', 'MainPage.html'));
+    res.sendFile(path.join(__dirname, 'Public', 'index.html')); // Asegúrate de que este archivo exista
+});
+
+// Manejo básico de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 // Iniciar el servidor
